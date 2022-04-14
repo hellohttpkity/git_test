5822efa46c305cb5be748e147bf919c21717b713
cxb <2664761650@q.com>
2022-04-14 19:27:48 +0800

    use idea to third commit

--- src/main/java/com/cxb/git/HelloGit.java
+++ src/main/java/com/cxb/git/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
         System.out.println("hello git!");
         System.out.println("hello github!");
         System.out.println("hello gitee!");
+        System.out.println("hello git3!");
     }
 }
