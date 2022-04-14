ed38160ee3b4b1309b648be7bc48f1b06cf4391b
cxb <2664761650@q.com>
2022-04-14 19:26:20 +0800

    use idea to second commit

--- src/main/java/com/cxb/git/HelloGit.java
+++ src/main/java/com/cxb/git/HelloGit.java
@@ -9,5 +9,7 @@ package com.cxb.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello github!");
+        System.out.println("hello gitee!");
     }
 }
