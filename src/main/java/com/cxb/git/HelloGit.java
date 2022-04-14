0c548af82b7d84c472b2458c897782632e374f9a
cxb <2664761650@q.com>
2022-04-14 19:21:44 +0800

    use idea to commit

---
+++ src/main/java/com/cxb/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.cxb.git;
+
+/**
+ * @Description:
+ * @author: xiwen
+ * @create: 2022-04-14 19:16
+ * @Version: 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
