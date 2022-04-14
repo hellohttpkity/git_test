4ae2601ead894dbc89c535feb101f391045fc9db
cxb <2664761650@q.com>
2022-04-14 19:46:54 +0800

    hotfix test

--- src/main/java/com/cxb/git/HelloGit.java
+++ src/main/java/com/cxb/git/HelloGit.java
@@ -13,5 +13,6 @@ public class HelloGit {
         System.out.println("hello gitee!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
     }
 }
