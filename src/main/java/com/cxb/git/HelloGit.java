bc67d20ff8ca462d19073f305b82f52e99b8be4d
cxb <2664761650@q.com>
2022-04-14 19:36:52 +0800

    hot-fix commit1

--- src/main/java/com/cxb/git/HelloGit.java
+++ src/main/java/com/cxb/git/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("hello github!");
         System.out.println("hello gitee!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
     }
 }
